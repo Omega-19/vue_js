91dd08743660c97a66d8a06409ff36c97d9dc25a
Omega-19 <vigannicole1@gmail.com>
2023-07-12 18:22:20 +0100

    Delete 7_le_prototype directory

--- 7_le_prototype/prototpe.js
+++
@@ -1,9 +0,0 @@
-let Person = {
-    firstname : "John",
-    lastname :  "Doe",
-    age : 25,
-   present : function(){
-        return `je m'appelle ${this.firstname} ${this.lastname} et j'ai ${this.age}`
-    }
-}
-console.log(Person.present());
