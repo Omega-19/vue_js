08696cdd7ced5a0fd16d3e6e29b773dd8726fa63
Omega-19 <vigannicole1@gmail.com>
2023-07-12 18:22:55 +0100

    Delete objets_classes_js directory

--- objets_classes_js/scriptA.js
+++
@@ -1,5 +0,0 @@
-`use strict`
-let x = 5;
-
-export let z = 200;
-
--- objets_classes_js/scriptB.js
+++
@@ -1,5 +0,0 @@
-`use strict`
-import { z as zScriptA } from "./scriptA.js"//utilisé les 
-let y = 10;
-let z = 20 + zScriptA;
-console.log(z);
